ddb6144fbb7cb95420655062718e92fda931fe5a
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2018-03-04 11:36:47 +0100

    integrated animation

--- src/js/app.js
+++ src/js/app.js
@@ -11,9 +11,12 @@ import 'slick-carousel';
 
 import rb_image from "./responsive_background_images"
 
+import paper from 'paper'
+paper.install(window);
 
-var Snap = require("imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js");
+import gridAnimation from './grid'
 
+var paperjsAniamtion= new gridAnimation();
 
 $(document).ready(function() {
 
@@ -25,28 +28,17 @@ $(document).ready(function() {
 		offset: 20,
 	});
 
-	// //animate the grid when in view
-	// inView('#slide_2')
-	// 	.on('enter', function() {
-	// 		console.log('enter');
-	// 		moveGridUp();
-	// 	}).on('exit', function() {
-	// 		console.log('exit');
-	// 		moveGridDown();
-	// 	});
-
-
-		//start the video on init, must stay before slick init
-		$('.portfolio-carousel').on('init', function(ev, el) {
-			$('video').each(function() {
-				this.play();
-			});
+	//start the video on init, must stay before slick init
+	$('.portfolio-carousel').on('init', function(ev, el) {
+		$('video').each(function() {
+			this.play();
 		});
+	});
 
 	//init the portfolio carousel in home;
 	$('.portfolio-carousel').slick({
 		'dots': true,
-		'autoplay':true
+		'autoplay': true
 	});
 
 	//start loading the responsive_background_images
@@ -55,9 +47,8 @@ $(document).ready(function() {
 		new rb_image(elements[i]);
 	}
 
-
 	//enable info
-	$('#hero .wtf').click(function(){
+	$('#hero .wtf').click(function() {
 		$('#hero .wtf').toggleClass('expanded');
 	})
 
@@ -66,111 +57,14 @@ $(document).ready(function() {
 
 	var antefact = new Audio('/assets/antefact.mp3');
 
-	$('.playSound').click(function(){
+	$('.playSound').click(function() {
 		event.preventDefault();
-		antefact.play();	})
+		antefact.play();
+	})
+
+	paperjsAniamtion.init();
 
 });
-
-
-//require 'snapsvg/dist/snap.svg';
-//
-// var s = Snap("#dots-bg");
-// var grid = [];
-// var gridStep = 36;
-//
-// //here I calculate some numbers for creating the grid
-// var gridContainerW = $("#dots-bg").width();
-// var gridContainerH = $("#dots-bg").height();
-//
-//
-// if (gridContainerW > 600) {
-// 	gridStep = 36;
-// } else if (gridContainerW < 500) {
-// 	gridStep = 30;
-// }
-//
-// var rowCount = Math.floor(gridContainerW / gridStep);
-// var colCount = Math.floor(gridContainerH / gridStep);
-//
-// //make the container that need to snap to the grid same width as the gridStep
-// $('.grid-snap').width(gridStep * rowCount);
-//
-// for (var i = 0; i <= colCount; i++) {
-// 	for (var j = 0; j <= rowCount; j++) {
-// 		var dot = s.circle(gridStep * j, gridStep * i + 200, 2);
-// 		grid.push(dot);
-// 		dot.attr({
-// 			fill: '#2828E9'
-// 		})
-// 	}
-// }
-//
-// function moveGridUp() {
-// 	for (var i = 0; i < grid.length; i++) {
-// 		setTimeout(moveDotUp, i , i);
-// 	}
-// }
-//
-// function moveGridDown() {
-// 	for (var i = 0; i < grid.length; i++) {
-// 		setTimeout(moveDotDown, i , i);
-// 	}
-// }
-//
-// function moveDotUp(i) {
-// 	var dot = grid[i];
-// 	dot.animate({
-// 		cy: parseInt(dot.attr('cy')) - 200
-// 	}, 500);
-//
-// }
-//
-// function moveDotDown(i) {
-// 	var dot = grid[i];
-// 	dot.animate({
-// 		cy: parseInt(dot.attr('cy')) + 200
-// 	}, 500);
-// }
-//
-
-
-
-// ******************************
-//  il carosello fatto a manina
-// ******************************
-
-
-// var carousellOffset=0;
-//
-// function initCustomCarousel(){
-// 	var items=$('.portfolio-carousel').children().length;
-//
-// 	var singleItemWidth=$('.container').width();
-// 	$('.portfolio-carousel .slide').width(singleItemWidth);
-//
-// 	var marginRight=($(document).width()-singleItemWidth);
-// 	$('.portfolio-carousel .slide').css('margin-right',marginRight);
-// 	$('.portfolio-carousel').width((singleItemWidth+marginRight)*items+marginRight);
-//
-// 	carousellOffset=(((singleItemWidth+marginRight)*items)+marginRight)/items;
-//
-// 	createNavigationDots(items);
-// }
-//
-// function createNavigationDots(items){
-// 	for (var i=0; i<items; i++){
-// 		$('.dots').append("<li class='nav' id="+i+"></li>");
-// 	}
-// 	$('.dots .nav').click(function(){
-// 		slideCarousel($(this).attr('id'));
-// 	});
-// }
-//
-// function slideCarousel(target){
-// 	console.log(target)
-// 	$('.portfolio-carousel .slide').first().css('margin-left',-carousellOffset*target	 )
-// }
 
 
 
@@ -178,17 +72,17 @@ $(document).ready(function() {
  * HELPERS
  ----------------------------------------------------------------------- */
 function _hasClass(el, className) {
-  return el.classList ? el.classList.contains(className) : new RegExp('\\b'+ className+'\\b').test(el.className);
+	return el.classList ? el.classList.contains(className) : new RegExp('\\b' + className + '\\b').test(el.className);
 }
 
 function _addClass(el, className) {
-  if (el.classList) el.classList.add(className);
-  else if (!_hasClass(el, className)) el.className += ' ' + className;
+	if (el.classList) el.classList.add(className);
+	else if (!_hasClass(el, className)) el.className += ' ' + className;
 }
 
 function _removeClass(el, className) {
-  if (el.classList) el.classList.remove(className);
-  else el.className = el.className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
+	if (el.classList) el.classList.remove(className);
+	else el.className = el.className.replace(new RegExp('\\b' + className + '\\b', 'g'), '');
 }
 
 
@@ -197,86 +91,94 @@ function _removeClass(el, className) {
  ----------------------------------------------------------------------- */
 function ie9InputFix() {
 
-  var ua = navigator.userAgent.toLowerCase();
-  var isIE9 =
-    document.addEventListener &&
-    /msie/.test(ua) &&
-    parseFloat((ua.match(/.*(?:rv|ie)[\/: ](.+?)([ \);]|$)/) || [])[1]) < 10;
+	var ua = navigator.userAgent.toLowerCase();
+	var isIE9 =
+		document.addEventListener &&
+		/msie/.test(ua) &&
+		parseFloat((ua.match(/.*(?:rv|ie)[\/: ](.+?)([ \);]|$)/) || [])[1]) < 10;
 
-  if ( ! isIE9) return;
+	if (!isIE9) return;
 
-  var inputs = document.querySelectorAll('input, textarea');
+	var inputs = document.querySelectorAll('input, textarea');
 
-  function ie9TriggerInput(el) {
-    if (el.value !== el.getAttribute('data-oldValue')) {
-      var event = document.createEvent('HTMLEvents');
-      event.initEvent('input', true, false);
-      el.dispatchEvent(event);
-    }
-  }
+	function ie9TriggerInput(el) {
+		if (el.value !== el.getAttribute('data-oldValue')) {
+			var event = document.createEvent('HTMLEvents');
+			event.initEvent('input', true, false);
+			el.dispatchEvent(event);
+		}
+	}
 
-  for (var i = inputs.length - 1; i >= 0; i--) {
-    var el = inputs[i];
-    var selectionListener = (function(theEl) {
-      return function(e) { ie9TriggerInput(theEl); }
-    })(el);
-    el.addEventListener('focus', function() {
-      document.addEventListener('selectionchange', selectionListener, false);
-    });
-    el.addEventListener('blur', function() {
-      document.removeEventListener('selectionchange', selectionListener, false);
-    });
-    el.addEventListener('input', (function(theEl) {
-      return function() {
-        el.setAttribute('data-oldValue', el.value);
-      }
-    })(el));
-  }
+	for (var i = inputs.length - 1; i >= 0; i--) {
+		var el = inputs[i];
+		var selectionListener = (function(theEl) {
+			return function(e) {
+				ie9TriggerInput(theEl);
+			}
+		})(el);
+		el.addEventListener('focus', function() {
+			document.addEventListener('selectionchange', selectionListener, false);
+		});
+		el.addEventListener('blur', function() {
+			document.removeEventListener('selectionchange', selectionListener, false);
+		});
+		el.addEventListener('input', (function(theEl) {
+			return function() {
+				el.setAttribute('data-oldValue', el.value);
+			}
+		})(el));
+	}
 }
 
 function floatingLabels() {
 
-  var labels = document.getElementsByClassName('fp-floating-label');
+	var labels = document.getElementsByClassName('fp-floating-label');
 
-  function toggleClass(el, className, value) {
-    if (value) {
-      _addClass(el, className);
-    } else {
-      _removeClass(el, className);
-    }
-  }
+	function toggleClass(el, className, value) {
+		if (value) {
+			_addClass(el, className);
+		} else {
+			_removeClass(el, className);
+		}
+	}
 
-  function onFocus(label) {
-    toggleClass(label, 'fp-floating-label--focused', true);
-  }
+	function onFocus(label) {
+		toggleClass(label, 'fp-floating-label--focused', true);
+	}
 
-  function onBlur(label) {
-    toggleClass(label, 'fp-floating-label--focused', false);
-  }
+	function onBlur(label) {
+		toggleClass(label, 'fp-floating-label--focused', false);
+	}
 
-  function onInput(label, value) {
-    value = value.trim();
-    toggleClass(label, 'fp-floating-label--valued', value);
-  }
+	function onInput(label, value) {
+		value = value.trim();
+		toggleClass(label, 'fp-floating-label--valued', value);
+	}
 
-  for (var i = labels.length - 1; i >= 0; i--) {
-    var label = labels[i];
-    var input = label.nextElementSibling;
+	for (var i = labels.length - 1; i >= 0; i--) {
+		var label = labels[i];
+		var input = label.nextElementSibling;
 
-    input.addEventListener('focus', (function(theLabel) {
-      return function() { onFocus(theLabel); };
-    })(label));
+		input.addEventListener('focus', (function(theLabel) {
+			return function() {
+				onFocus(theLabel);
+			};
+		})(label));
 
-    input.addEventListener('blur', (function(theLabel) {
-      return function() { onBlur(theLabel); };
-    })(label));
+		input.addEventListener('blur', (function(theLabel) {
+			return function() {
+				onBlur(theLabel);
+			};
+		})(label));
 
-    input.addEventListener('input', (function(theLabel) {
-      return function() { onInput(theLabel, this.value); };
-    })(label));
+		input.addEventListener('input', (function(theLabel) {
+			return function() {
+				onInput(theLabel, this.value);
+			};
+		})(label));
 
-    onInput(label, input.value);
-  }
+		onInput(label, input.value);
+	}
 }
 
 ie9InputFix()
@@ -288,27 +190,27 @@ floatingLabels();
  ----------------------------------------------------------------------- */
 var advSelects = document.getElementsByClassName('fp-advanced-select');
 for (var i = advSelects.length - 1; i >= 0; i--) {
-  var $select = $(advSelects[i]);
-  (function($el) {
-    var $label = $el.prev();
-    $el.selectize({
-      // implement label magic for selectize
-      onFocus: function() {
-        $label.addClass('fp-floating-label--focused');
-      },
-      onBlur: function() {
-        $label.removeClass('fp-floating-label--focused');
-      },
-      onChange: function(value) {
-        value = value.trim();
-        if (value) {
-          $label.addClass('fp-floating-label--valued');
-        } else {
-          $label.removeClass('fp-floating-label--valued');
-        }
-      }
-    });
-  })($select);
+	var $select = $(advSelects[i]);
+	(function($el) {
+		var $label = $el.prev();
+		$el.selectize({
+			// implement label magic for selectize
+			onFocus: function() {
+				$label.addClass('fp-floating-label--focused');
+			},
+			onBlur: function() {
+				$label.removeClass('fp-floating-label--focused');
+			},
+			onChange: function(value) {
+				value = value.trim();
+				if (value) {
+					$label.addClass('fp-floating-label--valued');
+				} else {
+					$label.removeClass('fp-floating-label--valued');
+				}
+			}
+		});
+	})($select);
 }
 
 
@@ -318,21 +220,21 @@ for (var i = advSelects.length - 1; i >= 0; i--) {
 // Which transition event?
 // by David Walsh: http://davidwalsh.name/css-animation-callback
 var transitionEnd = (function() {
-  var el = document.createElement('fakeelement');
-  var transitions = {
-    'transition'      : 'transitionend',
-    'OTransition'     : 'oTransitionEnd',
-    'MozTransition'   : 'transitionend',
-    'WebkitTransition': 'webkitTransitionEnd'
-  }
+	var el = document.createElement('fakeelement');
+	var transitions = {
+		'transition': 'transitionend',
+		'OTransition': 'oTransitionEnd',
+		'MozTransition': 'transitionend',
+		'WebkitTransition': 'webkitTransitionEnd'
+	}
 
-  for (var t in transitions) {
-    if (el.style[t] !== undefined) {
-      return transitions[t];
-    }
-  }
+	for (var t in transitions) {
+		if (el.style[t] !== undefined) {
+			return transitions[t];
+		}
+	}
 
-  return 'transitionend';
+	return 'transitionend';
 })();
 
 
@@ -343,9 +245,9 @@ var transitionEnd = (function() {
  ----------------------------------------------------------------------- */
 function storeFields(form) {
 
-  var storedFields = [].slice.call(form.querySelectorAll('[data-store]'));
+	var storedFields = [].slice.call(form.querySelectorAll('[data-store]'));
 
-  storedFields.forEach(function(field) {
+	storedFields.forEach(function(field) {
 
 		var value;
 
@@ -363,11 +265,11 @@ function storeFields(form) {
 
 function setStoredFields() {
 
-  var storedFields = [].slice.call(document.querySelectorAll('[data-store]'));
+	var storedFields = [].slice.call(document.querySelectorAll('[data-store]'));
 
-  storedFields.forEach(function(field) {
+	storedFields.forEach(function(field) {
 
-    var stored = window.localStorage.getItem(field.id);
+		var stored = window.localStorage.getItem(field.id);
 
 		if (stored) {
 			if (field.type === 'radio' || field.type === 'checkbox') {
@@ -375,17 +277,17 @@ function setStoredFields() {
 			} else {
 				field.value = stored;
 
-        // special case for selectize
-        if (field.selectize) {
-          field.selectize.addItem(stored);
-        }
+				// special case for selectize
+				if (field.selectize) {
+					field.selectize.addItem(stored);
+				}
 			}
 			var event = document.createEvent('HTMLEvents');
 			event.initEvent('input', true, false);
 			field.dispatchEvent(event);
 		}
 
-  });
+	});
 
 }
 
@@ -395,8 +297,8 @@ setStoredFields();
 // [EXAMPLE] Save values on form submit
 var form = document.querySelector('.fp-form');
 form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  storeFields(form);
+	event.preventDefault();
+	storeFields(form);
 });
 
 
@@ -405,59 +307,54 @@ var textarea = document.querySelector('textarea');
 
 textarea.addEventListener('keydown', autosize);
 
-function autosize(){
+function autosize() {
 	console.log("resizing");
-  var el = this;
-  setTimeout(function(){
-    el.style.cssText = 'height:auto; padding:0';
-    // for box-sizing other than "content-box" use:
-    // el.style.cssText = '-moz-box-sizing:content-box';
-    el.style.cssText = 'height:' + el.scrollHeight + 'px';
-  },0);
+	var el = this;
+	setTimeout(function() {
+		el.style.cssText = 'height:auto; padding:0';
+		// for box-sizing other than "content-box" use:
+		// el.style.cssText = '-moz-box-sizing:content-box';
+		el.style.cssText = 'height:' + el.scrollHeight + 'px';
+	}, 0);
 }
 
-var lineSystem={} ;
+var lineSystem = {};
 
-function splitText(){
+function splitText() {
 	var splitted = $('.padded-multiline-container').wraplines();
 	var opts = $('.padded-multiline-container').data();
 
-	  $('.padded-multiline-container  span').addClass('extra');
-	  $('.padded-multiline-container').removeClass('init');
+	$('.padded-multiline-container  span').addClass('extra');
+	$('.padded-multiline-container').removeClass('init');
 }
 
 
-function shiftTextAnimation(){
+function shiftTextAnimation() {
 
-	var listLength=$('.changing-text ul li').length;
-	var offset=parseInt($('.changing-text ul').css('margin-top'));
-	if (offset<(listLength-1)*-60){
-		offset=0
+	var listLength = $('.changing-text ul li').length;
+	var offset = parseInt($('.changing-text ul').css('margin-top'));
+	if (offset < (listLength - 1) * -60) {
+		offset = 0
 	}
 
-	$('.changing-text ul').css('margin-top',offset-60+'px');
+	$('.changing-text ul').css('margin-top', offset - 60 + 'px');
 
-	$('#loop').toggleClass('rotate',true);
-	setTimeout(function(){
-		$('#loop').toggleClass('rotate',false);
-	},500);
+	$('#loop').toggleClass('rotate', true);
+	setTimeout(function() {
+		$('#loop').toggleClass('rotate', false);
+	}, 500);
 }
 
 
 // netlify form handling
 $("#contact-form").submit(function(e) {
-  e.preventDefault();
+	e.preventDefault();
 
-  var $form = $(this);
-  $.post($form.attr("action"), $form.serialize()).then(function() {
-    alert("Thank you!");
-  });
+	var $form = $(this);
+	$.post($form.attr("action"), $form.serialize()).then(function() {
+		alert("Thank you!");
+	});
 });
-
-
-
-
-
 
 
 /*
@@ -479,12 +376,12 @@ jQuery.fn.wraplines = function(options) {
 		offsetLeft: 0
 	}, options);
 	return this.each(function() {
-		options.index  = 0;
+		options.index = 0;
 		options.offset = 0;
 		var parentElm = $(this);
 		var elmText = $(parentElm).text();
 		$(parentElm).html(function(ind, htm) {
-			var $repText = '<' + options.lineWrap + '>' + elmText.replace( /\s/g, '</' + options.lineWrap + '> <' + options.lineWrap + '>');
+			var $repText = '<' + options.lineWrap + '>' + elmText.replace(/\s/g, '</' + options.lineWrap + '> <' + options.lineWrap + '>');
 			$repText = $repText + '</' + options.lineWrap + '>';
 			return $repText;
 		});
@@ -498,14 +395,14 @@ jQuery.fn.wraplines = function(options) {
 		});
 		for (var x = 1; x <= options.index; x++) {
 			$('.' + options.wordClassPrefix + x, parentElm)
-			.wrapAll('<' + options.lineWrap + ' class="line ' + options.lineClassPrefix + x + '" />')
-			.append(" ");
+				.wrapAll('<' + options.lineWrap + ' class="line ' + options.lineClassPrefix + x + '" />')
+				.append(" ");
 			var innerText = $('.' + options.lineClassPrefix + x, parentElm).text();
 			$('.' + options.lineClassPrefix + x, parentElm).html(function() {
 				return innerText;
 			});
-		lineSystem.index=options.index
+			lineSystem.index = options.index
 
-    }
+		}
 	});
 };
---
+++ src/js/grid.js
@@ -0,0 +1,84 @@
+class gridAnimation {
+
+	constructor(height, width) {
+
+		this.canvas = document.getElementById('paperjsAniamtion');
+		paper.setup(this.canvas);
+
+		this.height = view.size.height;
+		this.width = view.size.width;
+
+		this.words = [];
+
+		this.contents = ['think', 'play', 'code', 'test', 'shit!', 'repeat', 'research', 'design', 'prototype', 'meh!', 'repeat', 'detail', 'polish', 'deliver', 'finally!', 'repeat'];
+
+		this.numberOfRows = 30; //determine the number of rows we want
+		this.numberOfColumns = 30; //determine the number of columns we want
+		this.offset = 30;
+		this.xStep = this.width / this.numberOfColumns; //to make sure they are evenly spaced, we divide the width and height by numberOfColumns
+		this.yStep = this.height / this.numberOfRows; //and numberOfRows respectively
+
+	}
+
+	init() {
+
+		for (var x = this.offset; x < this.width - this.offset; x += this.xStep) { //start at the first column, where x = 0
+			for (var y = this.offset; y < this.height - this.offset; y += this.yStep) { //go through all the rows (y = 0, y = yStep * 1, y  = yStep * 2, etc.)
+				var delay = getRandomInt(500, 5000);
+				this.words.push(new Text(x, y, delay));
+			}
+		}
+
+		view.onFrame = (event) => {
+			var millis = event.time * 1000;
+
+			for (var i = 0; i < this.words.length; i++) {
+				if (millis - this.words[i].text.lastchange > this.words[i].text.delay) {
+					this.words[i].animate(this.contents);
+					this.words[i].text.lastchange = millis;
+				} else {}
+			}
+		}
+	}
+}
+
+function getRandomInt(min, max) {
+	var interval = max - min;
+	var range = Math.floor(Math.random() * Math.floor(interval));
+	var translated = min + range;
+	return (translated);
+}
+
+function Text(px, py, delay) {
+	this.text = new PointText();
+
+	this.text.position.x = px;
+	this.text.position.y = py;
+	this.text.fontFamily = 'Roboto Mono';
+	this.text.fontSize = 8;
+	this.text.fillColor = '#ADADAD';
+	this.text.content = 'ante:fact';
+	this.text.delay = delay;
+	this.text.lastchange = 0;
+	this.text.currentIndex = 0;
+}
+
+Text.prototype = {
+
+	animate: function(contents) {
+
+		if (this.text.currentIndex == 1 || this.text.currentIndex == 5 || this.text.currentIndex == 9) {
+			this.text.fillColor = '#0000FF';
+		} else {
+			this.text.fillColor = '#ADADAD';
+		}
+		if (this.text.currentIndex == contents.length - 1) {
+			this.text.currentIndex = 0;
+		} else {
+			this.text.currentIndex++;
+		}
+		this.text.content = contents[this.text.currentIndex];
+	}
+}
+
+export default gridAnimation;
